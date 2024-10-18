0d1c4e59d16f514fe1ac46b6de3c3a014cd36c02
theon87 <theonfrancis17@gmail.com>
2024-10-18 16:10:28 -0400

    adds index.ts to model folder

--- server/src/model/index.ts
+++ server/src/model/index.ts
@@ -1,1 +1,12 @@
-import sequelize from '../config/connection';
+import sequelize from '../config/connection.js';
+// import recipe factory function
+
+
+// Initialize the models
+
+// const Recipe = RecipeFactory(sequelize);
+
+// Create associations between the models
+
+
+export { sequelize, Recipe };
