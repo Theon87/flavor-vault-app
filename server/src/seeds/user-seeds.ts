4c1a5770c92f281e24969ee65edd39547aa7336c
kurlee710 <josephatamaniuk@gmail.com>
2024-10-18 14:12:50 -0400

    added two additional users to users-seeds

--- server/src/seeds/user-seeds.ts
+++ server/src/seeds/user-seeds.ts
@@ -9,6 +9,16 @@ export const seedUsers = async () => {
         email: "kurlee@bootcamp.com",
         password: "password123",
       },
+      {
+        username: "JSmith",
+        email: "j.smith@gmail.com",
+        password: "password456",
+      },
+      {
+        username: "Pamela.b",
+        email: "p.bay@aol.com",
+        password: "password789",
+      },
     ],
     { individualHooks: true }
   );
