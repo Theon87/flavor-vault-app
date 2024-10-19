1de88f58ac318481552bd984aad0cdc7a8dc5f36
theon87 <theonfrancis17@gmail.com>
2024-10-19 18:07:15 -0400

    uncomments seeds index.ts with only seedUsers

--- server/src/seeds/index.ts
+++ server/src/seeds/index.ts
@@ -1,23 +1,23 @@
-// import { seedUsers } from "./user-seeds.js";
+import { seedUsers } from "./user-seeds.js";
 // import { seedRecipes } from "./recipe-seeds.js";
-// import sequelize from "../config/connection.js";
+import sequelize from "../config/connection.js";
 
-// const seedAll = async (): Promise<void> => {
-//   try {
-//     await sequelize.sync({ force: true });
-//     console.log("Database synced successfully.");
+const seedAll = async (): Promise<void> => {
+  try {
+    await sequelize.sync({ force: true });
+    console.log("Database synced successfully.");
 
-//     await seedUsers();
-//     console.log("Users seeded successfully.");
+    await seedUsers();
+    console.log("Users seeded successfully.");
 
-//     await seedRecipes();
-//     console.log("Recipes seeded successfully.");
+    // await seedRecipes();
+    // console.log("Recipes seeded successfully.");
 
-//     process.exit(0);
-//   } catch (error) {
-//     console.error("Error seeding data:", error);
-//     process.exit(1);
-//   }
-// };
+    process.exit(0);
+  } catch (error) {
+    console.error("Error seeding data:", error);
+    process.exit(1);
+  }
+};
 
-// seedAll();
+seedAll();
