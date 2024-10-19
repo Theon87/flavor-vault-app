b67178e45b1e841bd78736937c0f61aa56ea0485
Jeronimo Toro <jeronimotoro@Luisas-MBP.lan>
2024-10-19 13:37:54 -0400

    linked home on app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
-
+import Home from "./pages/Home"
 import './App.css';
 
 function App() {
   return (
-  <div className='App'>
-    <h1 class="text-3xl font-bold underline">Hello world!</h1>
-      </div>
+   <div className='bg-black'>
+     <Home />
+   </div>
   );
 }
 export default App;
