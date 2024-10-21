67f3a06d5acb7b118dfc5eee77449a751451c1f9
Jeronimo Toro <jeronimotoro@Luisas-MBP.lan>
2024-10-21 13:26:14 -0400

    added optional home.css style in case file is created

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,6 +2,7 @@
 //This willl be our home page
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Home.css'; // Optional: Import CSS for styling
 //We will need a header, a nav bar containg home, login, and my recipes.
 
 //We will need a search bar to search for recipes on the page.
