7d34a77c0f4184052739e314bf908297d4ae279f
Paola Nicole Ayala <paolanayala@gmail.com>
2024-10-21 10:59:21 -0400

    working on establishing routes from main to all offered pages, wip

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,16 +1,39 @@
 import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// createRoot(document.getElementById('root')!).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+import my-recipies from './pages/my-recipies.tsx';
+import Home from './pages/home.tsx';
+import Login from './pages/Login.tsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Recipe />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      }, 
+      {
+        path: '/login',
+        element: <Login />
+      }, 
+    ]
+  }
+])
 const rootElement = document.getElementById('root');
 if (rootElement) {
-  ReactDOM.createRoot(rootElement).render(<RouterProvider router={} />);
+  ReactDOM.createRoot(rootElement).render(<RouterProvider router={router} />);
 }
