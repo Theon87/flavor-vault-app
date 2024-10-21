4e582c2d8f25a5473351ce699d81086f26cbc0fc
Aditi Rai <aditi45@gmail.com>
2024-10-22 04:56:36 +0530

    added recipecard component for home page

--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -22,24 +22,21 @@ const Navbar = () => {
   const [active,setActive]  = useState(0)
   return (
     <header>
-  <div className="container mx-auto flex flex-wrap px-5 py-3 flex-col md:flex-row items-center">
-    <Link className="flex  items-center mb-4 md:mb-0" to={"/"}>
-    <img src={ logo } alt="logo" width={50}/>
-      {/* <span className="ml-3 text-xl">Recipe Finder</span> */}
-    </Link>
-    <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-     {
-       links.map((link,index)=>{
+      <div className="container mx-auto flex flex-wrap px-5 py-3 flex-col md:flex-row items-center">
+        <Link className="flex  items-center mb-4 md:mb-0" to={"/"}>
+          <img src={ logo } alt="logo" width={50}/>
+        </Link>
+      <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+        {
+        links.map((link,index)=>{
         return  <Link className={`mr-5 ${active==index ? 'border-b-2 border-green-500':''}`} onClick={()=>setActive(index)} to={link.path} key={index} >{link.name}</Link>
-     
-       
-       })
-     }
+        })
+        }
     
-    </nav>
+      </nav>
     
-  </div>
-</header>
+      </div>
+    </header>
 
   )
 }
---
+++ client/src/components/RecipeCard.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const RecipeCard = () => {
+  return (
+    <div>RecipeCard</div>
+  )
+}
+
+export default RecipeCard
