eb986ac9b8be0c48b81315c1dc5bde690e906010
theon87 <theonfrancis17@gmail.com>
2024-10-21 13:48:26 -0400

    removes unused auth route from api index.ts

--- server/src/routes/api/index.ts
+++ server/src/routes/api/index.ts
@@ -4,17 +4,14 @@ import { myRecipesRouter } from './my-recipes-routes.js';
 import { userRouter } from './user-route.js';
 
 import { groceryListRouter } from './grocery-list-routes.js';
-// import { login } from './auth-routes'; 
 
 
 const router = express.Router();
 
-// // router.use('/login', loginRouter);
 router.use('/recipes', myRecipesRouter);
 router.use('/users', userRouter);
 
 router.use('/grocery-list', groceryListRouter);
-// router.use('/auth', login);
 
 
 export default router;
