20a1dca0752394573cd4a3c6a100f60a32051ea8
Sahiramarte17 <sahiramarte17@gmail.com>
2024-10-21 09:07:27 -0400

    adding import grocerylist router

--- server/src/routes/api/index.ts
+++ server/src/routes/api/index.ts
@@ -2,6 +2,7 @@
 
 import express from 'express';
 import { myRecipesRouter } from './my-recipes-routes.js';
+import { groceryListRouter } from './grocery-list-routes';
 
 const router = express.Router();
 
