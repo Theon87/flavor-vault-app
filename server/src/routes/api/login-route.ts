2adae0dab2248e857b72e4a908f84d9fa7d4ec07
Jeronimo Toro <jeronimotoro@Luisas-MBP.lan>
2024-10-21 10:31:59 -0400

    login route function

--- server/src/routes/api/login-route.ts
+++ server/src/routes/api/login-route.ts
@@ -2,3 +2,10 @@
 import express, { Request, Response } from 'express';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
+
+const router = express.Router();
+
+// Login route
+router.post('/login', async (req: Request, res: Response) => {
+    const { username, password } = req.body;
+  
