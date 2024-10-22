b0d08fb2c81ff22633ab19c85a57d23957e60c64
theon87 <theonfrancis17@gmail.com>
2024-10-21 23:33:33 -0400

    removes a logo from login page

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -37,7 +37,7 @@ const Login = () => {
 
   return (
     <>
-    <Navigation />
+      <Navigation />
       {/*
         This example requires updating your template:
 
@@ -51,11 +51,6 @@ const Login = () => {
       </div>
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img
-            alt="Flavor Vault"
-            src="./flavor-vault-logo.png"
-            className="mx-auto h-10 w-auto"
-          />
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
             Sign in to your account
           </h2>
