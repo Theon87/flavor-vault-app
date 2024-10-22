7dd10af98b8c794c864723a6199e97b6cdd45d37
Paola Nicole Ayala <paolanayala@gmail.com>
2024-10-21 22:08:30 -0400

    added get route for grocery items by id

--- server/src/routes/api/grocery-list-routes.ts
+++ server/src/routes/api/grocery-list-routes.ts
@@ -14,7 +14,22 @@ router.get('/', async (_req: Request, res: Response) => {
     res.status(500).json({ message: error.message }); // Handle errors
   }
 });
+ 
+// GET /api/grocery-list/:id - Retrieve a grocery list item by id
+router.get('/:id', async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    const groceryItem = await GroceryList.findByPk(id);
 
+    if (!groceryItem) {
+      return res.status(404).json({ message: 'Item not found' });
+    }
+    console.log(`GET /grocery-list/${id} - Item Retrieved`, groceryItem);
+    res.json(groceryItem);
+  } catch (error: any) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 export { router as groceryListRouter }; // Export the router
 
