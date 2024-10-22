fa3237bd3b85b97d20bbbeca4a473c7ff7854c09
kurlee710 <josephatamaniuk@gmail.com>
2024-10-22 01:17:00 -0400

    copied code for index.ts from branch 174 because I was unable to resolve conflicts in 174.

--- server/src/model/index.ts
+++ server/src/model/index.ts
@@ -1,24 +1,53 @@
-import sequelize from '../config/connection.js';
-import { RecipeFactory } from './recipe.js';
-import { UserFactory } from './user.js';
-import { GroceryListFactory } from './groceryList.js'; // Import the GroceryListFactory
+import sequelize from "../config/connection.js";
+import { RecipeFactory } from "./recipe.js";
+import { UserFactory } from "./user.js";
+import { GroceryListFactory } from "./groceryList.js";
 
 // Initialize the models
 const Recipe = RecipeFactory(sequelize);
 const User = UserFactory(sequelize);
-const GroceryList = GroceryListFactory(sequelize); // Initialize GroceryList
+const GroceryList = GroceryListFactory(sequelize);
 
 // Create associations between the models
-User.hasOne(GroceryList, {
-    onDelete: 'CASCADE',
-});
 
-Recipe.belongsTo(User);
-
+// 1. A User can have many Recipes (One-to-Many)
 User.hasMany(Recipe, {
-    onDelete: 'CASCADE',
+  foreignKey: "userId",
+  as: "recipes", // Alias for accessing related recipes
+  onDelete: "CASCADE", // Delete the user's recipes if the user is deleted
+});
+Recipe.belongsTo(User, {
+  foreignKey: "userId",
+  as: "user", // Alias for accessing the recipe's user
 });
 
-export { Recipe, User, GroceryList }; // Export the GroceryList model
+// 2. A User can have many Grocery List Items (One-to-Many)
+User.hasMany(GroceryList, {
+  foreignKey: "userId",
+  as: "groceryLists",
+  onDelete: "CASCADE",
+});
+GroceryList.belongsTo(User, {
+  foreignKey: "userId",
+  as: "user",
+});
 
+// 3. A Recipe can belong to many Grocery List Items (Many-to-Many)
+Recipe.belongsToMany(GroceryList, {
+  through: "RecipeGroceryList", // This will create a join table 'RecipeGroceryList'
+  foreignKey: { name: "recipeId", allowNull: false },
+  as: "groceryLists", // Alias for accessing related grocery lists
+});
+GroceryList.belongsToMany(Recipe, {
+  through: "RecipeGroceryList",
+  foreignKey: { name: "groceryListId", allowNull: false },
+  as: "recipes",
+});
 
+// Sync the models with the database
+sequelize
+  .sync({ alter: true })
+  .then(() => console.log("Models synchronized with the database."))
+  .catch((err) => console.error("Error syncing models:", err));
+
+export { Recipe, User, GroceryList };
