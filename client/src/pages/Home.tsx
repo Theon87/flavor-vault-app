6744127569bac386324fbccd56ea82a3454e5585
Paola Nicole Ayala <paolanayala@gmail.com>
2024-10-22 08:58:22 -0400

    homepage file polished, API endpoint added in myRecipes within pages on client side

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -6,15 +6,6 @@ import '../index.css';
 import SearchBar from '../components/SearchBar';
 import RecipeCard from '../components/RecipeCard';
 
-//We will need a header, a nav bar containg home, login, and my recipes.
-
-//We will need a search bar to search for recipes on the page.
-
-// Once we have the search bar we will need to display the recipes that match the search pulled from our api.
-
-// When the user clicks on a recipe we will need to display the recipe details on a seperate page (recipe.tsx).
-
-// Finally we'll need the footer
 
 const Home = () => {
     return (
