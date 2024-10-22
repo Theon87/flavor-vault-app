af6ee5663a8ac7bcdd6895f9c74bf33be2c0860e
theon87 <theonfrancis17@gmail.com>
2024-10-22 02:17:02 -0400

    adds middleware to api route

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,9 +1,10 @@
+import { authenticateToken } from '../middleware/auth.js';
 import apiRoutes from './api/index.js'
 import authRoutes from './auth-routes.js'
 import express from 'express';
 const router = express.Router();
 
-router.use('/api', apiRoutes);
+router.use('/api', authenticateToken, apiRoutes);
 router.use('/auth', authRoutes);
 
 export default router;
