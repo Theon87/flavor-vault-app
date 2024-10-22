d68d4b10b48b1b251a0c0aff92a8ce9295dc22f8
Aditi Rai <aditi45@gmail.com>
2024-10-22 19:41:51 +0530

    centering card

--- client/src/components/RecipeCard.tsx
+++ client/src/components/RecipeCard.tsx
@@ -16,19 +16,21 @@ const saveRecipe = () => {
 };
 
 return (
-  <Card >
-  <img className="max-w-sm rounded overflow-hidden shadow-lg"
+  <Card className="max-w-sm mx-auto rounded overflow-hidden shadow-lg bg-white p-6 mt-10">
+  <img
     src="https://img.spoonacular.com/recipes/716429-556x370.jpg"
     alt="Pasta with Garlic, Scallions, Cauliflower & Breadcrumbs"
   />
     <CardHeader color="black" className="relative h-56">
     Pasta with Garlic, Scallions, Cauliflower & Breadcrumbs
+    <br></br>
+    <Typography variant="h5" color="blue-gray" className="mb-2">
+      Discover mouthwatering Pasta recipes.
+      </Typography>
     </CardHeader>
 
     <CardBody>
-      <Typography variant="h5" color="blue-gray" className="mb-2">
-      Discover mouthwatering Pasta recipes.
-      </Typography>
+
     </CardBody>
     <CardFooter className="pt-0 flex justify-between">
       {/* Save Button */}
