ded5ac81824d0ca3c51034683a7b1404f4f8200e
Aditi Rai <aditi45@gmail.com>
2024-10-23 03:12:45 +0530

    final changes

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect} from "react";
 import auth from "../utils/auth";
 import Navigation from '../components/Navigation';
 import Header from '../components/Header';
@@ -7,6 +7,7 @@ import '../index.css';
 import SearchBar from '../components/SearchBar';
 import RecipeCard from '../components/RecipeCard';
 import ErrorPage from './ErrorPage';
+// import Login from './Login';
 
 const Home = () => {
     const [error, setError] = useState(false);
@@ -40,84 +41,10 @@ const Home = () => {
 
     if (!isLoggedIn) {
         return (
-            <>
             <div>
                 <h1>Please login to view recipes</h1>
             </div>
-            <div>
-            {/* <div> */}
-              {/* <Navigation /> */}
-            {/* </div> */}
-            <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-              <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-                  Sign in to your account
-                </h2>
-              </div>
-      
-              <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-                <form action="#" method="POST" className="space-y-6" onSubmit={handleSubmit}>
-                  <div>
-                    <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                      Username
-                    </label>
-                    <div className="mt-2">
-                      <input
-                        id="username"
-                        name="username"
-                        type="text"
-                        value={loginData.username || ''}
-                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        onChange={handleChange}
-                      />
-                    </div>
-                  </div>
-      
-                  <div>
-                    <div className="flex items-center justify-between">
-                      <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                        Password
-                      </label>
-                      <div className="text-sm">
-                        <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                          Forgot password?
-                        </a>
-                      </div>
-                    </div>
-                    <div className="mt-2">
-                      <input
-                        id="password"
-                        name="password"
-                        type="password"
-                        required
-                        autoComplete="current-password"
-                        value={loginData.password || ''}
-                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        onChange={handleChange}
-                      />
-                    </div>
-                  </div>
-      
-                  <div>
-                    <button
-                      type="submit"
-                      className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                    >
-                      Sign in
-                    </button>
-                  </div>
-                </form>
-      
-                <p className="mt-10 text-center text-sm text-gray-500">
-                  Not a member?{' '}
-                  <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-                    Sign up now
-                  </a>
-                </p>
-              </div>
-            </div>
-          </div>
-          </>
+            
         );
     } 
 
